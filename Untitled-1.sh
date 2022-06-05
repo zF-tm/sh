58c1ebe38d47a69c58ac9a3844338b1429bd867d
zF-tm <73698972+zF-tm@users.noreply.github.com>
2022-06-05 11:16:19 +0300

    Add files via upload

---
+++ Untitled-1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+x="0"
+
+while [ $x = 0 ] do
+    clear
+
+    echo "Choose Ur Mom: 
+1. lana rhodes
+2.mia khalifa
+3. ba3rafesh"
+
+read Mom
+
+case "$mom" in
+    1)
+        type="lana"
+        Sex=20
+        mommy=40;;
+    2)
+        type="mia"
+        Sex=40
+        mommy=90;;
+    3)
+        type="ba3rafesh"
+        Sex=900
+        mommy=90;; 
+esac
+done
